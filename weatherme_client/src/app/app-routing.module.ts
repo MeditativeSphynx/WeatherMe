37ec223cd768208940f7ee65d4949cb6f621a74a
Daniel <meditativesphynx@gmail.com>
2023-08-22 01:28:20 -0400

    fix path for route

--- weatherme_client/src/app/app-routing.module.ts
+++ weatherme_client/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { WeatherComponent } from './components/weather/weather.component';
 
 const routes: Routes = [
-  { path: '/', component: WeatherComponent }
+  { path: '', component: WeatherComponent }
 ];
 
 @NgModule({
